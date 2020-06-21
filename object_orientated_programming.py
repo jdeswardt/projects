1df47abe6ac2d858fe1a01663d384e43c48fb711
Jaco de Swardt <jdeswardt@2u.com>
2020-06-21 21:27:55 +0200

    [GJ] Adding script

---
+++ object_orientated_programming.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 21 21:16:31 2020
+
+@author: jdeswardt
+"""
+
+class calculation:
+    
+    def __init__(self):
+        pass
+    
+    def summation(self, element1, element2):
+        return element1 + element2
+    
+    def subtraction(self, element1, element2):
+        return element1 - element2
+    
+    
+inst = calculation()
+    
+inst.subtraction(100, 50)
+
+calculation.summation(1, 100, 50)
